7458bb209cb4360c238024863ab3db5918ddd0e1
ahuda620 <ahuda620@gmail.com>
2024-10-09 03:02:32 -0700

    Added ReactQuery Dev tools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
 import MobileProvider from "./context/MobileContext";
 import HomePage from "./pages/HomePage";
-import "./index.css";
+import JobSearchPage from "./pages/JobSearchPage";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "/jobs",
+    element: <JobSearchPage />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MobileProvider>
-      <RouterProvider router={router} />
-    </MobileProvider>
+    <QueryClientProvider client={queryClient}>
+      <MobileProvider>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </MobileProvider>
+    </QueryClientProvider>
   </StrictMode>
 );

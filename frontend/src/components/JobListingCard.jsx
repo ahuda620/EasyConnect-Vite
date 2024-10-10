33f5617b5494602c92abc33904290d95a9b35cf0
ahuda620 <ahuda620@gmail.com>
2024-10-09 20:58:58 -0700

    mostly finished styles for job listing card component

--- frontend/src/components/JobListingCard.jsx
+++ frontend/src/components/JobListingCard.jsx
@@ -1,8 +1,11 @@
 import styles from "./JobListingCard.module.css";
 import { useQuery } from "@tanstack/react-query";
-import { getJobListings } from "../api/apis";
+import { getJobListings } from "../util/apis";
+import dateFormatter from "../util/dateFormatter";
+import keywordExtractor from "../util/keywordExtractor";
 
-export default function JobListingCard() {
+export default function JobListingCard({ handleJobSelect }) {
+  //Fetch Job Listings
   const {
     isLoading: isJobListingsLoading,
     error: jobListingsError,
@@ -22,26 +25,49 @@ export default function JobListingCard() {
     return <span>Error fetching job listings</span>;
   }
 
+  //Change style of select job listing card container on click
+  function handleJobCardClick(event) {
+    const element = event.currentTarget; // The clicked job card
+
+    // Remove the 'clicked' class from all job cards
+    document
+      .querySelectorAll(`.${styles.jobListingCardWrapper}`)
+      .forEach((card) => {
+        card.classList.remove(styles.clicked);
+      });
+
+    // Add the 'clicked' class to the clicked job card
+    element.classList.add(styles.clicked);
+  }
+
   return (
     <>
       {jobListings.data.map((jobListing) => {
-        //Convert joblistng unix timestamps to month/day/year
-        const timestamp = jobListing.job_posted_at_timestamp;
-        const date = new Date(timestamp * 1000); // Convert to milliseconds
-        const month = date.getMonth() + 1; // Months are 0-indexed
-        const day = date.getDate();
-        const year = date.getFullYear();
-        const formattedDate = `${month}/${day}/${year}`;
+        jobListing.posted_date_formatted = dateFormatter(jobListing);
+        jobListing.job_keywords = keywordExtractor(
+          jobListing.job_highlights.Qualifications
+        );
 
         //Only display at max 3 job keywords on job listing card to mitigate overflow
-        // const limitedJobKeywords = jobListing.job_keywords.slice(0, 3);
+        const limitedJobKeywords = jobListing.job_keywords.slice(0, 3);
 
         return (
           <article
             key={jobListing.job_id}
+            onClick={(event) => {
+              handleJobSelect(jobListing);
+              handleJobCardClick(event);
+            }}
             className={styles.jobListingCardWrapper}
           >
-            <img src={jobListing.employer_logo}></img>
+            {jobListing.employer_logo ? (
+              <img
+                src={jobListing.employer_logo}
+                className={styles.employerLogo}
+              ></img>
+            ) : (
+              <div className={styles.employerLogoPlaceholder}></div>
+            )}
             <div className={styles.jobDetailsWrapper}>
               <h3 className={styles.jobListingTitle}>{jobListing.job_title}</h3>
               <p className={styles.jobListingEmployerName}>
@@ -49,15 +75,15 @@ export default function JobListingCard() {
               </p>
               <p className={styles.jobListingLocation}>{`${
                 jobListing.job_city
-              }${jobListing.job_state ? `, ${jobListing.job_state}` : ""} ${
-                jobListing.job_county ? `, ${jobListing.job_county}` : ""
+              }${jobListing.job_state ? `, ${jobListing.job_state}` : ""}${
+                jobListing.job_country ? `, ${jobListing.job_country}` : ""
               }`}</p>
               {/* Job description highlights */}
-              {/* <ul className={styles.jobListingKeywords}>
+              <ul className={styles.jobListingKeywords}>
                 {limitedJobKeywords.map((keyword, index) => {
                   return <li key={index}>{keyword}</li>;
                 })}
-              </ul> */}
+              </ul>
               <ul className={styles.jobListingPreviewText}>
                 {jobListing.job_highlights.Responsibilities.map(
                   (listItem, index) => (
@@ -66,7 +92,7 @@ export default function JobListingCard() {
                 )}
               </ul>
               <p className={styles.jobListingPostedDate}>
-                Posted: {formattedDate}
+                {jobListing.posted_date_formatted}
               </p>
             </div>
           </article>

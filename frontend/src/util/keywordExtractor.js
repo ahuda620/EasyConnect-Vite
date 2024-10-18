4844c8c97ed90a6400cf7ae3afca59061f6068d8
ahuda620 <ahuda620@gmail.com>
2024-10-17 20:09:14 -0700

    not using keywordExtractor atm, will probably remove in future

--- frontend/src/util/keywordExtractor.js
+++ frontend/src/util/keywordExtractor.js
@@ -1,15 +1,45 @@
 import nlp from "compromise";
 
-export default function keywordExtractor(qualifications) {
-  return qualifications
+export default function keywordExtractor(
+  qualifications,
+  responsibilities,
+  jobDescription
+) {
+  let keywords = qualifications
     .map((qualification) => {
       //Extract keywords from each sentence/element of the qualifications array
-      const doc = nlp(qualification);
+      let doc = nlp(qualification);
+      let extractedWords = doc.topics().out("array");
 
-      const keywords = doc.topics().out("array");
-      return keywords
-        .map((keyword) => keyword.replace(/,/g, "").trim()) // Remove commas and trim whitespace
-        .filter((keyword) => keyword.trim().length > 0); //Remove any empty arrays
+      return extractedWords
+        .map((extractedWord) => extractedWord.replace(/,/g, "").trim()) // Remove commas and trim whitespace
+        .filter((extractedWord) => extractedWord.trim().length > 0); //Remove any empty arrays
     })
     .flat(); //Combine all arrays into one array
+
+  //If no keywords were extracted from qualifications, try responsibilities
+  if (keywords.length === 0) {
+    keywords = responsibilities
+      .map((responsibilities) => {
+        //Extract keywords from each sentence/element of the qualifications array
+        let doc = nlp(responsibilities);
+        let extractedWords = doc.topics().out("array");
+
+        return extractedWords
+          .map((extractedWord) => extractedWord.replace(/,/g, "").trim()) // Remove commas and trim whitespace
+          .filter((extractedWord) => extractedWord.trim().length > 0); //Remove any empty arrays
+      })
+      .flat(); //Combine all arrays into one array
+  }
+
+  //If no keywords were extracted from responsibilities, fall back to job description
+  if (keywords.length === 0) {
+    let doc = nlp(jobDescription);
+    keywords = doc
+      .topics()
+      .out("array")
+      .map((extractedWord) => extractedWord.replace(/,/g, "").trim()) // Remove commas and trim whitespace
+      .filter((extractedWord) => extractedWord.trim().length > 0); //Remove any empty arrays
+  }
+  return keywords;
 }

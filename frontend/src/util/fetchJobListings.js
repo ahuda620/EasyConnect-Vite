b4306d5d64fe5e76a4f941a6e7abd63b4277a501
ahuda620 <ahuda620@gmail.com>
2024-10-29 13:07:09 -0700

    changed api endpoint url

--- frontend/src/util/fetchJobListings.js
+++ frontend/src/util/fetchJobListings.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const VM_IP = import.meta.env.VITE_VM_IP;
+
 export default async function (searchParamObject) {
   console.log(searchParamObject);
-  const response = await axios.get("http://localhost:4000/api/jobs/");
+  const response = await axios.get(`http://${VM_IP}/api/jobs/`);
+  // const response = await axios.get("http://192.168.1.24:4000/api/jobs/");
 
   return response.data.data;
 }

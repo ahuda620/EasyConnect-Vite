9289aea9532170c7c07c9e55cfe3e0437e630468
ahuda620 <ahuda620@gmail.com>
2024-10-29 15:07:04 -0700

    added utility function to match user skills with job skills

---
+++ frontend/src/util/jobSkillsMatcher.js
@@ -0,0 +1,19 @@
+import Fuse from "fuse.js";
+
+export default function jobSkillsMatcher(userSkills, jobDescription) {
+  const jobDescriptionWords = jobDescription.split(" ");
+
+  const fuse = new Fuse(jobDescriptionWords, {
+    threshold: 0.1,
+    distance: 100,
+    findAllMatches: true,
+  });
+
+  const matchingSkills = userSkills.filter(
+    (skill) => fuse.search(skill.toLowerCase()).length > 0
+  );
+
+  console.log(matchingSkills);
+
+  return matchingSkills;
+}

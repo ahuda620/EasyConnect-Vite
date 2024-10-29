4b25f32f1877ce890916034f582e06fc2d41a64f
ahuda620 <ahuda620@gmail.com>
2024-10-29 15:23:14 -0700

    job details panel now displays matching user skills if any

--- frontend/src/components/JobListingDetail.jsx
+++ frontend/src/components/JobListingDetail.jsx
@@ -1,33 +1,101 @@
+import sanitizeJobDescription from "../util/sanitizeJobDescription";
 import styles from "./JobListingDetail.module.css";
-import parse from "html-react-parser";
+import jobSkillsMatcher from "../util/jobSkillsMatcher";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  faBookmark,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function JobListingDetail({ selectedJob }) {
+export default function JobListingDetail({ userSkills, selectedJob }) {
   const jobListing = selectedJob;
 
-  return jobListing ? (
-    <article key={jobListing.job_id} className={styles.jobListingDetailWrapper}>
-      <img src={jobListing.employer_logo}></img>
-      <div className={styles.jobDetailsWrapper}>
-        <h3 className={styles.jobListingTitle}>{jobListing.job_title}</h3>
-        <p className={styles.jobListingEmployerName}>
-          {jobListing.employer_name}
-        </p>
-        <p className={styles.jobListingLocation}>{`${jobListing.job_city}${
-          jobListing.job_state ? `, ${jobListing.job_state}` : ""
-        }${jobListing.job_country ? `, ${jobListing.job_country}` : ""}`}</p>
+  //display if theres any user skills matching with the job description
+  let matchedSkills;
 
-        <hr className={styles.customLineBreak} />
+  if (userSkills && jobListing) {
+    matchedSkills = jobSkillsMatcher(userSkills, jobListing.job_description);
+  }
+
+  return jobListing ? (
+    <article key={jobListing.job_id} className={styles.wrapper}>
+      <div className={styles.jobDetailsHeader}>
+        <div className={styles.companyInfo}>
+          <img src={jobListing.employer_logo}></img>
+          <span className={styles.jobListingEmployerName}>
+            {jobListing.employer_name}
+          </span>
+        </div>
+        <h3 className={styles.jobListingTitle}>{jobListing.job_title}</h3>
+        <p className={styles.jobListingLocation}>
+          {`${
+            jobListing.job_city && jobListing.job_state
+              ? `${jobListing.job_city}, ${jobListing.job_state}`
+              : jobListing.job_city
+              ? `${jobListing.job_city}, ${jobListing.job_country}`
+              : jobListing.job_state
+              ? `${jobListing.job_state}, ${jobListing.job_country}`
+              : jobListing.job_country
+          }`}
+          {jobListing.job_is_remote ? " (Remote)" : ""}
+        </p>
+        <p className={styles.jobListingPostedDate}>
+          {jobListing.posted_date_formatted}
+        </p>
+
+        <ul className={styles.jobListingKeywords}>
+          <li>
+            {jobListing.job_employment_type &&
+            jobListing.job_employment_type === "FULLTIME"
+              ? "Full-time"
+              : jobListing.job_employment_type === "PARTTIME"
+              ? "Part-time"
+              : jobListing.job_employment_type === "CONTRACTOR"
+              ? "Contractor"
+              : jobListing.job_employment_type === "INTERN"
+              ? "Internship"
+              : ""}
+          </li>
+          {jobListing.job_is_remote && <li>Remote</li>}
+          {matchedSkills.map((skill) => (
+            <li key={skill} className={styles.matchedSkills}>
+              {/* Captilize first letter of each word of matched skill */}
+              {skill
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+              <FontAwesomeIcon icon={faCheck} className={styles.faCheck} />
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.jobListingLinks}>
+          <a href="#" className={styles.jobListingApplyLink}>
+            Apply
+            <FontAwesomeIcon
+              icon={faArrowUpRightFromSquare}
+              className={styles.faApplyIcon}
+            />
+          </a>
+          <FontAwesomeIcon icon={faBookmark} className={styles.faSaveIcon} />
+        </div>
+      </div>
+
+      <div className={styles.jobDetailsBody}>
         {jobListing.job_highlights &&
-        jobListing.job_highlights.Benefits &&
-        jobListing.job_highlights.Benefits.length > 0 ? (
+        jobListing.job_highlights.Responsibilities &&
+        jobListing.job_highlights.Responsibilities.length > 0 ? (
           <>
-            <h4 className={styles.jobListingHeaderText}>Benefits</h4>
-            <ul className={styles.jobListingBenefits}>
-              {jobListing.job_highlights.Benefits.map((benefit, index) => {
-                return <li key={index}>{benefit}</li>;
-              })}
+            <h4 className={styles.sectionHeaderText}>Responsibilities</h4>
+            <ul className={styles.list}>
+              {jobListing.job_highlights.Responsibilities.map(
+                (responsibility, index) => {
+                  return <li key={index}>{responsibility}</li>;
+                }
+              )}
             </ul>
-            <hr className={styles.customLineBreak} />
+            <hr className={styles.lineBreak} />
           </>
         ) : (
           <></>
@@ -36,53 +104,44 @@ export default function JobListingDetail({ selectedJob }) {
         jobListing.job_highlights.Qualifications &&
         jobListing.job_highlights.Qualifications.length > 0 ? (
           <>
-            <h4 className={styles.jobListingHeaderText}>Qualifications</h4>
-            <ul className={styles.jobListingQualifications}>
+            <h4 className={styles.sectionHeaderText}>Qualifications</h4>
+            <ul className={styles.list}>
               {jobListing.job_highlights.Qualifications.map(
                 (qualification, index) => {
                   return <li key={index}>{qualification}</li>;
                 }
               )}
             </ul>
-            <hr className={styles.customLineBreak} />
+            <hr className={styles.lineBreak} />
           </>
         ) : (
           <></>
         )}
         {jobListing.job_highlights &&
-        jobListing.job_highlights.Responsibilities &&
-        jobListing.job_highlights.Responsibilities.length > 0 ? (
+        jobListing.job_highlights.Benefits &&
+        jobListing.job_highlights.Benefits.length > 0 ? (
           <>
-            <h4 className={styles.jobListingHeaderText}>Responsibilities</h4>
-            <ul className={styles.jobListingResponsibilities}>
-              {jobListing.job_highlights.Responsibilities.map(
-                (responsibility, index) => {
-                  return <li key={index}>{responsibility}</li>;
-                }
-              )}
+            <h4 className={styles.sectionHeaderText}>Benefits</h4>
+            <ul className={styles.list}>
+              {jobListing.job_highlights.Benefits.map((benefit, index) => {
+                return <li key={index}>{benefit}</li>;
+              })}
             </ul>
-            <hr className={styles.customLineBreak} />
+            <hr className={styles.lineBreak} />
           </>
         ) : (
           <></>
         )}
         {jobListing.job_description ? (
           <>
-            <h4 className={styles.jobListingHeaderText}>
-              Full Job Description
-            </h4>
+            <h4 className={styles.sectionHeaderText}>About the job</h4>
             <p className={styles.jobListingDescription}>
-              {parse(jobListing.job_description)}
+              {sanitizeJobDescription(jobListing.job_description)}
             </p>
-            <hr className={styles.customLineBreak} />
           </>
         ) : (
           <></>
         )}
-
-        <p className={styles.jobListingPostedDate}>
-          {jobListing.posted_date_formatted}
-        </p>
       </div>
     </article>
   ) : (

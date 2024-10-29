35431162d1acf2b76368454fe940e6ae9e15cf4f
ahuda620 <ahuda620@gmail.com>
2024-10-29 12:57:15 -0700

    added utility function to fetch user skills from backend

---
+++ frontend/src/util/fetchUserSkills.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const VM_IP = import.meta.env.VITE_VM_IP;
+
+export default async function (userId) {
+  const response = await axios.get(`http://${VM_IP}/api/user/${userId}/skills`);
+  return response;
+}

fdd59ca3690fb6b8eb86b793a48fd301a0cbc6e5
ahuda620 <ahuda620@gmail.com>
2024-11-03 18:27:06 -0800

    abstracted clerk route controller into seperate file

--- backend/routes/clerkRoutes.js
+++ backend/routes/clerkRoutes.js
@@ -1,67 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Webhook } from "svix";
-import { clerkClient } from "@clerk/express";
-import db from "../database/db.js";
+import { handleClerkWebHook } from "../controllers/clerkController.js";
 
 export const router = express.Router();
 
 router.post(
   "/",
   bodyParser.raw({ type: "application/json" }),
-  async (req, res) => {
-    const payload = req.body;
-    const headers = req.headers;
-
-    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
-    let msg;
-    try {
-      //Verify webhook using Svix
-      //Webhook is triggered by Clerk user.created and session.created events
-      msg = wh.verify(payload, headers); //var that stores verified request body message
-
-      //Extract user_id from webhook
-      const userId = msg.data.user_id;
-
-      let user; //var that stores fetched user data from Clerk API
-      try {
-        //Fetch user data from Clerk API
-        user = await clerkClient.users.getUser(userId);
-      } catch (error) {
-        console.error("Error getting user data from Clerk API");
-        return res.status(400).json({ error: error.message });
-      }
-
-      //Check if user is in database
-      try {
-        const [rows] = await db.query("SELECT * FROM users WHERE id = ?", [
-          userId,
-        ]);
-
-        if (rows.length === 0) {
-          //If user is not in database, insert them in
-          try {
-            await db.query("INSERT INTO users (id, first_name) VALUES (?, ?)", [
-              userId,
-              user.firstName,
-            ]);
-            return res.sendStatus(201);
-          } catch (error) {
-            console.error("Error inserting new user into database");
-            return res.status(500).json({ error: error.message });
-          }
-        } else {
-          //User is already in database
-          return res.sendStatus(200);
-        }
-      } catch (error) {
-        console.error("Error checking database:", error);
-        return res.status(500).json({ error: error.message });
-      }
-    } catch (error) {
-      return res
-        .status(400)
-        .json({ message: `Webhook verfication failed: ${error}` });
-    }
-  }
+  handleClerkWebHook
 );

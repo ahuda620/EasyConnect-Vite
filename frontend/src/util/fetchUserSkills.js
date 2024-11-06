6af909f0260055439afb14502826970909bd3cc9
ahuda620 <ahuda620@gmail.com>
2024-11-06 13:03:12 -0800

    changed response data format

--- frontend/src/util/fetchUserSkills.js
+++ frontend/src/util/fetchUserSkills.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const VM_IP = import.meta.env.VITE_VM_IP;
 
 export default async function (userId) {
-  const response = await axios.get(`http://${VM_IP}/api/user/${userId}/skills`);
-  return response;
+  const response = await axios.get(
+    `http://${VM_IP}/api/user/${userId}/getSkills`
+  );
+
+  return response.data;
 }

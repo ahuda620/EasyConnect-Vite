d9f6eacd7c98fd5641960b8e18630b1e083d615a
ahuda620 <ahuda620@gmail.com>
2024-11-06 12:54:06 -0800

    removed req.auth console log

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,8 +1,5 @@
 import jobListings from '../data/jobListings.json' with { type: 'json' };
 
 export const getJobListings = async (req, res) => {
-    if(req.auth){
-        console.log(req.auth);
-    }
     res.json(jobListings);
 }

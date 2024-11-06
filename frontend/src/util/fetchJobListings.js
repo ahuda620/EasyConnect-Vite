2e587238bc857857d50682413f20fe43f65a4b44
ahuda620 <ahuda620@gmail.com>
2024-11-06 14:47:30 -0800

    removed old request url

--- frontend/src/util/fetchJobListings.js
+++ frontend/src/util/fetchJobListings.js
@@ -5,7 +5,6 @@ const VM_IP = import.meta.env.VITE_VM_IP;
 export default async function (searchParamObject) {
   console.log(searchParamObject);
   const response = await axios.get(`http://${VM_IP}/api/jobs/`);
-  // const response = await axios.get("http://192.168.1.24:4000/api/jobs/");
 
   return response.data.data;
 }

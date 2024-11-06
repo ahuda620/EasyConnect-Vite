4ba2e94a5519b632120a56e3595f272f99260400
ahuda620 <ahuda620@gmail.com>
2024-11-06 12:55:54 -0800

    added endpoints to fetch and update user skills in database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,39 @@
+import db from "../database/db.js";
 import user from '../data/mockUser.json' with { type: 'json' };
 
-export const getUserSkills = async (req, res) => {
+export const updateUserSkills = async(req,res) => {
+    const {userId} = req.params;
+    const { skills } = req.body;
+
+    //Validate skills array
+    if(!skills || !Array.isArray(skills) || skills.length === 0 || !userId){
+        return res.status(400).send({error:"Invalid skills format"});
+    }
+
+    //update user skills in database
+    try {
+        const [rows] = await db.query("UPDATE users SET skills = ? WHERE id = ?", [JSON.stringify(skills), userId]);
+        return res.status(200).send("Skills updated in database successfully");
+    } catch (error) {
+        console.error("Error updating user skills in database");
+        return res.status(500).send(error.message);
+    }
+
+}
+
+export const getUserSkills = async(req, res) => {
     const userId = req.params.userId;
+
+    try {
+        const [rows] = await db.query("SELECT skills FROM users WHERE id = ?", [userId]);
+
+        const skills = JSON.parse(rows[0].skills);
+
+        res.status(200).send(skills);
+    } catch (error) {
+        console.error("Error fetching user skills in database");
+        return res.status(500).send(error.message);
+    }
+
     
-    res.json(user.skills);
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getUserSkills } from "../controllers/userController.js";
+import { updateUserSkills } from "../controllers/userController.js";
 
 export const router = express.Router();
 
-router.get(`/:userId/skills`, getUserSkills);
+router.get("/:userId/getSkills", getUserSkills);
+router.patch("/:userId/updateSkills", updateUserSkills);

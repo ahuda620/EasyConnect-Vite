b6868e79e81fa590c6f10125c98cd5ddde0a2cd8
ahuda620 <ahuda620@gmail.com>
2024-11-12 21:46:30 -0800

    added commented debug for lazy scrolling

--- frontend/src/util/fetchJobListings.js
+++ frontend/src/util/fetchJobListings.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const VM_IP = import.meta.env.VITE_VM_IP;
 
 export default async function (searchParamObject) {
-  console.log(searchParamObject);
+  console.log(searchParamObject); //TODO: delete jobSearchTerm and locationSearchTerm properties before sending to api
   const response = await axios.get(`http://${VM_IP}/api/jobs/`);
 
+  // if (Number(searchParamObject.page) > 1) {
+  //   return [];
+  // } else {
+  //   return response.data.data;
+  // }
   return response.data.data;
 }

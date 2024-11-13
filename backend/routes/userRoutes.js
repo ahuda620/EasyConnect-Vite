558d2d619a935588cb4f7fe9ba39ec51c0c20e11
ahuda620 <ahuda620@gmail.com>
2024-11-12 21:26:40 -0800

    added routes saved job listings

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { getUserSkills } from "../controllers/userController.js";
 import { updateUserSkills } from "../controllers/userController.js";
+import { saveJobListing } from "../controllers/userController.js";
+import { getSavedJobIds } from "../controllers/userController.js";
+import { deleteSavedJobId } from "../controllers/userController.js";
 
 export const router = express.Router();
 
 router.get("/:userId/getSkills", getUserSkills);
 router.patch("/:userId/updateSkills", updateUserSkills);
+router.post("/:userId/saveJobListing", saveJobListing);
+router.get("/:userId/getSavedJobIds", getSavedJobIds);
+router.patch("/:userId/deleteSavedJobId", deleteSavedJobId);

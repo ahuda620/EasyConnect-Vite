4229c0fdc81f73796f8b1f56fdda125e0cdc2126
ahuda620 <ahuda620@gmail.com>
2024-11-12 21:24:30 -0800

    added getJobDetails route

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { getJobListings } from "../controllers/jobController.js";
+import { getJobDetails } from "../controllers/jobController.js";
 
 export const router = express.Router();
 
+//These are routes for MOCK DATA
 router.get("/", getJobListings);
+router.get("/fetchJobDetails", getJobDetails);

b4e1cbcbf1aa6ac807a408a075f378620e384225
ahuda620 <ahuda620@gmail.com>
2024-11-19 20:55:58 -0800

    add ui changes for mobile

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,22 +3,19 @@ import SearchBar from "../components/SearchBar";
 
 export default function HomePage() {
   return (
-    <>
-      <div className={styles.pageWrapper}>
-        <div className={styles.heroContainer}>
-          <h1 className={styles.heroTitle}>
-            Easy Connect
-            <br /> Your Next Job, Simplified.
-          </h1>
-          <h2 className={styles.heroText}>
-            Find jobs that match your skills.
-            <br /> Connect with top employers and grow your career.
-          </h2>
-        </div>
-        <div className={styles.searchBarWrapper}>
-          <SearchBar></SearchBar>
-        </div>
+    <div className={styles.pageWrapper}>
+      <div className={styles.heroContainer}>
+        <h1 className={styles.heroTitle}>
+          Easy Connect
+          <br /> Your Next Job, Simplified.
+        </h1>
+        <h2 className={styles.heroText}>
+          Find jobs that match your skills.
+          <br /> Connect with top employers and grow your career.
+        </h2>
       </div>
-    </>
+
+      <SearchBar></SearchBar>
+    </div>
   );
 }

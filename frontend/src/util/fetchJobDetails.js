00e3c355c3ba467acc0e02025c974e9ebad1b419
ahuda620 <ahuda620@gmail.com>
2024-11-19 22:11:13 -0800

    using real api routes

--- frontend/src/util/fetchJobDetails.js
+++ frontend/src/util/fetchJobDetails.js
@@ -11,36 +11,36 @@ export default async function (jobId) {
   console.log("Fetching job with id: ", jobId);
 
   /* MOCK DATA API REQUEST */
-  const response = await axios.get(`http://${vmIp}/api/jobs/fetchJobDetails`, {
-    params: {
-      jobIds: jobId,
-      extended_publisher_details: "true",
-      markup_job_description: "true",
-    },
-  });
-
-  return response.data;
-
-  /* ACTUAL API REQUEST */
-  // const options = {
-  //   method: "GET",
-  //   url: "https://jsearch.p.rapidapi.com/job-details",
+  // const response = await axios.get(`http://${vmIp}/api/jobs/fetchJobDetails`, {
   //   params: {
-  //     job_id: jobId,
+  //     jobIds: jobId,
   //     extended_publisher_details: "true",
   //     markup_job_description: "true",
   //   },
-  //   headers: {
-  //     "x-rapidapi-key": `${rapidApiKey}`,
-  //     "x-rapidapi-host": "jsearch.p.rapidapi.com",
-  //   },
-  // };
+  // });
 
-  // try {
-  //   const response = await axios.request(options);
+  // return response.data;
 
-  //   return response.data.data;
-  // } catch (error) {
-  //   console.error("Error fetching job details:", error);
-  // }
+  /* ACTUAL API REQUEST */
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/job-details",
+    params: {
+      job_id: jobId,
+      extended_publisher_details: "true",
+      markup_job_description: "true",
+    },
+    headers: {
+      "x-rapidapi-key": `${rapidApiKey}`,
+      "x-rapidapi-host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching job details:", error);
+  }
 }
--- frontend/src/util/fetchJobListings.js
+++ frontend/src/util/fetchJobListings.js
@@ -4,35 +4,34 @@ const vmIp = import.meta.env.VITE_VM_IP;
 const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
 export default async function (searchParamObject) {
-  console.log(searchParamObject); //TODO: delete jobSearchTerm and locationSearchTerm properties before sending to api
+  // console.log(searchParamObject);
   const { initialSearch, jobSearchTerm, locationSearchTerm, ...params } =
     searchParamObject;
 
   /* MOCK DATA API REQUEST */
-  try {
-    const response = await axios.get(`http://${vmIp}/api/jobs/`);
-    console.log(response);
-    return response.data.data;
-  } catch (error) {
-    console.error(error);
-  }
-
-  /* ACTUAL API REQUEST */
-  // const options = {
-  //   method: "GET",
-  //   url: "https://jsearch.p.rapidapi.com/search",
-  //   params,
-  //   headers: {
-  //     "x-rapidapi-key": `${rapidApiKey}`,
-  //     "x-rapidapi-host": "jsearch.p.rapidapi.com",
-  //   },
-  // };
-
   // try {
-  //   const response = await axios.request(options);
-  //   console.log("Jobs:", response.data.data);
+  //   const response = await axios.get(`http://${vmIp}/api/jobs/`);
+  //   console.log(response);
   //   return response.data.data;
   // } catch (error) {
-  //   console.error("Error fetching job listings:", error);
+  //   console.error(error);
   // }
+
+  /* ACTUAL API REQUEST */
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/search",
+    params,
+    headers: {
+      "x-rapidapi-key": `${rapidApiKey}`,
+      "x-rapidapi-host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching job listings:", error);
+  }
 }

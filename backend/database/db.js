8332fadc16f8e77b7ec1c8f64f1a27c195203782
ahuda620 <ahuda620@gmail.com>
2024-11-20 18:42:49 -0800

    added db port

--- backend/database/db.js
+++ backend/database/db.js
@@ -9,9 +9,13 @@ let db;
 try {
   db = mysql.createPool({
     host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
   });
 } catch (error) {
   console.log(error);

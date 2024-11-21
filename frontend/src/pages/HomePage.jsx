13316dd30e8b6a39b1bf587d7591f250e1abcf47
ahuda620 <ahuda620@gmail.com>
2024-11-21 13:56:01 -0800

    fixed homepage background on mobile

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
 import styles from "./HomePage.module.css";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useIsMobile } from "../context/MobileContext";
 import SearchBar from "../components/SearchBar";
 
 export default function HomePage() {
+  const location = useLocation();
+  const isMobile = useIsMobile();
+
+  /*effect to fix homepage background not taking up full height on mobile*/
+  useEffect(() => {
+    if (isMobile && location.pathname === "/") {
+      document.documentElement.classList.add("overflow-visible"); //make overflow visible on root element
+      document.body.classList.add("overflow-visible");
+    } else {
+      document.documentElement.classList.remove("overflow-visible"); //make overflow visible on root element
+      document.body.classList.remove("overflow-visible");
+    }
+  }, [isMobile, location.pathname]);
+
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.heroContainer}>
